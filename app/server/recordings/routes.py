a78ee8321e2ffd27b174ab76391d0d419124c4c5
007vedant <raghuvedant00@gmail.com>
2021-12-09 14:06:12 +0530

    refactored post api routes

--- app/server/recordings/routes.py
+++ app/server/recordings/routes.py
@@ -1,7 +1,5 @@
-import os
-from server.config import Config
-from server.utils import token_required, allowed_file
-from flask import request, redirect, abort, jsonify, Blueprint
+from server.utils import token_required, allowed_file, upload_file
+from flask import request, jsonify, Blueprint
 
 recordings = Blueprint("recordings", __name__)
 
@@ -13,9 +11,7 @@ def upload(_id):
     if file and allowed_file(file.filename):
         ext = file.filename.rsplit(".", 1)[1].lower()
         filename = str(_id) + "." + ext
-        path = os.path.join(Config.UPLOAD_DIR, str(_id))
-        os.makedirs(path, exist_ok=True)
-        file.save(os.path.join(path, filename))
+        upload_file(file, filename, _id)
         return jsonify(filename), 200
 
     return jsonify("Invalid request."), 400
