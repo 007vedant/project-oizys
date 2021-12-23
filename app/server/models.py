8dd561d215c492c38cdca34c31e44a7341e15423
007vedant <raghuvedant00@gmail.com>
2021-12-23 23:03:57 +0530

    update user model with demographic and score fields

--- app/server/models.py
+++ app/server/models.py
@@ -1,6 +1,12 @@
-from typing_extensions import Required
 from bson import ObjectId
-from mongoengine import Document, StringField, EmailField, ObjectIdField
+from mongoengine import (
+    Document,
+    StringField,
+    EmailField,
+    ObjectIdField,
+    IntField,
+    BooleanField,
+)
 
 
 class User(Document):
@@ -8,5 +14,11 @@ class User(Document):
     name = StringField(max_length=20, required=True)
     email = EmailField(required=True)
     password = StringField(required=True)
+    age = IntField(required=True)
+    gender = StringField(required=True)
+    phq_score = IntField()
+    gad_score = IntField()
+    strp_score = IntField()
+    eligible = BooleanField(default=True)
 
     meta = {"collection": "user"}
