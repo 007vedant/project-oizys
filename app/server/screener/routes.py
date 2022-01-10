bbbeefb97e0c013be0000171dedce4c8f4043cdb
007vedant <raghuvedant00@gmail.com>
2022-01-11 00:04:37 +0530

    :sparkles: Added endpoints for pss questionnaire and reaction time tests.

--- app/server/screener/routes.py
+++ app/server/screener/routes.py
@@ -1,7 +1,7 @@
 from bson import ObjectId
 from server.models import User
 from flask import request, jsonify, Blueprint
-from server.utils import token_required, get_score
+from server.utils import token_required, get_score, get_pss_score
 
 screener = Blueprint("screener", __name__)
 
@@ -14,12 +14,40 @@ def screening(_id):
     phq_score = get_score(resp, 9, 17)
 
     user = User.objects(_id=ObjectId(_id)).first()
-    user.gad_scores.append(gad_score)
-    user.phq_scores.append(phq_score)
+    user.gad_score = gad_score
+    user.phq_score = phq_score
 
     if gad_score >= 8 or phq_score >= 15:
         user.eligible = False
 
+    user.save()
+
+    return jsonify("Success"), 201
+
+
+@screener.route("/api/pssscreening", methods=["POST"])
+@token_required
+def pssscreening(_id):
+    resp = request.json
+    pss_score = get_pss_score(resp, 1, 10)
+
+    user = User.objects(_id=ObjectId(_id)).first()
+    user.pss_scores.append(pss_score)
+    user.save()
+
+    return jsonify("Success"), 201
+
+
+@screener.route("/api/reactscreening", methods=["POST"])
+@token_required
+def reactscreening(_id):
+    resp = request.json
+    strp_score = resp["strp"]
+    brt_score = resp["brt"]
+
+    user = User.objects(_id=ObjectId(_id)).first()
+    user.strp_scores.append(strp_score)
+    user.brt_scores.append(brt_score)
     user.save()
 
     return jsonify("Success"), 201
