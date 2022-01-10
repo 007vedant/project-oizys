bd88ccde130aacb9abdfa5b80034587bb6fc6982
007vedant <raghuvedant00@gmail.com>
2022-01-10 23:53:55 +0530

    :sparkles: Added screen with meditation info with updated youtube link.

---
+++ app/client/src/screens/MeditationScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
+
+const MeditationScreen = () => {
+  return (
+    <Jumbotron>
+      <h1>Hello!</h1>
+      <p>Please meditate twice in a day while playing this <strong><a href="https://youtu.be/5ElrZxISPsc" target="_blank" rel="noreferrer">Youtube Video</a></strong>.</p>
+    </Jumbotron>
+  );
+};
+
+export default MeditationScreen;
