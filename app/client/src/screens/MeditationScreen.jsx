63162763ef82700d348321c477bb72ce361ee417
007vedant <raghuvedant00@gmail.com>
2022-01-14 17:55:57 +0530

    :wrench: updated youtube redirect url

--- app/client/src/screens/MeditationScreen.jsx
+++ app/client/src/screens/MeditationScreen.jsx
@@ -5,7 +5,7 @@ const MeditationScreen = () => {
   return (
     <Jumbotron>
       <h1>Hello!</h1>
-      <p>Please meditate twice in a day while playing this <strong><a href="https://youtu.be/5ElrZxISPsc" target="_blank" rel="noreferrer">Youtube Video</a></strong>.</p>
+      <p>Please meditate twice in a day while playing this <strong><a href="https://www.youtube.com/watch?v=LtX0px9ktiI" target="_blank" rel="noreferrer">Youtube Video</a></strong>.</p>
     </Jumbotron>
   );
 };
